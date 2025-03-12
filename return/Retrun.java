b660de68da1bd9e3984a01b7451e21920b6c0dad
francissusma <susmafrancis2005@gmail.com>
2025-03-12 14:40:54 +0530

    return type

---
+++ return/Retrun.java
@@ -0,0 +1,11 @@
+public class Retrun
+{
+    public int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String args[]){
+    Retrun obj=new Retrun();
+    int c=obj.add(10,20);
+     System.out.println(c);
+    }
+}
