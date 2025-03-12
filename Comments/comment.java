edf6c21086726560686a9e1c538e3a9e6c4d3193
francissusma <susmafrancis2005@gmail.com>
2025-03-12 14:13:46 +0530

    fourth code

--- Comments/comment.java
+++ Comments/comment.java
@@ -1,3 +1,7 @@
-hello
-susma
-jesii
+public class comment
+{
+    public static void main(String args[]){
+        int numOne=38;//this is the integer value
+        System.out.println(numOne);
+    }
+}
