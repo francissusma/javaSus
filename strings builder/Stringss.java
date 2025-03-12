731fc07ef8725bde10f71911846ad186173ce9c1
francissusma <susmafrancis2005@gmail.com>
2025-03-12 15:37:55 +0530

    string builder

---
+++ strings builder/Stringss.java
@@ -0,0 +1,8 @@
+public class Stringss{
+    public static void main(String args[])
+    {
+        String s1 = "susma";
+        String s2 = "francis";
+        System.out.println(s1+s2);
+            }
+}
