424fa6946cde505f09685a6c3d61fcf3ea8f77b3
francissusma <susmafrancis2005@gmail.com>
2025-03-13 15:02:00 +0530

    scanner done

---
+++ Scanner/Scanners.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Scanners
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your name:");
+        String name=sc.nextLine();
+        System.out.println("my name is:" +name);
+    }
+}
