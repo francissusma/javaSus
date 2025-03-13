390c2a7351426dd9baf04076d27254ec4c867b27
francissusma <susmafrancis2005@gmail.com>
2025-03-13 15:24:17 +0530

    userinput

---
+++ UserInput/Addition.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Addition
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter number1:");
+        int number1=s.nextInt();
+        System.out.println("enter number2:");
+        int number2=s.nextInt();
+        System.out.println(number1+number2);
+
+    }
+}
