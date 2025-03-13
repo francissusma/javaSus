21dbe1b685d4037f602f9ddcd3fda874b5a930d7
francissusma <susmafrancis2005@gmail.com>
2025-03-13 11:41:38 +0530

    odd even finded

---
+++ OddEven/OddEven.java
@@ -0,0 +1,16 @@
+public class OddEven
+{
+    public static void main(String args[])
+    { 
+        int num=25;
+        if(num%2==0)
+        {
+            System.out.println("it is even");
+        }
+        else
+        {
+            System.out.println("it is odd");
+        }
+    }
+
+}
