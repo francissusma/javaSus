19d1215039a8cbf8cc46b5614ec9fd6a9aee4e65
francissusma <susmafrancis2005@gmail.com>
2025-03-13 10:24:02 +0530

    arithmatic

---
+++ ArithmaticOperator/Arithmatic.java
@@ -0,0 +1,11 @@
+public class Arithmatic
+{
+    public static void main(String args[])
+    {
+        System.out.println(38+45);
+        System.out.println(20-10);
+        System.out.println(30*7);
+        System.out.println(20/2);
+        System.out.println(10%2);
+            }
+}
