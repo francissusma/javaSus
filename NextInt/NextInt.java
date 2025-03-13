39dfc2958caf4135328e2a05ff46742cb29bc959
francissusma <susmafrancis2005@gmail.com>
2025-03-13 15:09:57 +0530

    nextnt

---
+++ NextInt/NextInt.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class NextInt
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your age:");
+        int age=sc.nextInt();
+        System.out.println("my age is:" +age);
+    }
+}
