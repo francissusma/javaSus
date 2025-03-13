4725c5d6f8a610ef2f3bd99684f2f494973362d1
francissusma <susmafrancis2005@gmail.com>
2025-03-13 11:48:01 +0530

    else if done

---
+++ ElseIf/ElseIf.java
@@ -0,0 +1,19 @@
+public class ElseIf
+{
+    public static void main(String args[])
+    { 
+        int num=25;
+        if(num>0)
+        {
+            System.out.println("it is positive");
+        }
+        else if(num==0)
+        {
+            System.out.println("it is neutral");}
+        else{
+                System.out.println("it is negative");
+            }
+        
+    }
+
+}
