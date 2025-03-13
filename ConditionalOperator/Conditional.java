1260ca8f3ae5c4afc37ccbf8a7ec07d9cb1ef7e0
francissusma <susmafrancis2005@gmail.com>
2025-03-13 10:31:40 +0530

    conditional

---
+++ ConditionalOperator/Conditional.java
@@ -0,0 +1,12 @@
+public class Conditional
+{
+    public static void main(String args[])
+    {
+        System.out.println(38<45);
+        System.out.println(45>38);
+        System.out.println(5==5);
+        System.out.println(45!=7);
+        System.out.println(3<=4);
+        System.out.println(67>=78);
+    }
+}
