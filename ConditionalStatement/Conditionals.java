eaee0bf8336a9355ce68e0aa2b4a839800c98d37
francissusma <susmafrancis2005@gmail.com>
2025-03-13 10:48:52 +0530

    conditionalss

---
+++ ConditionalStatement/Conditionals.java
@@ -0,0 +1,13 @@
+public class Conditionals
+{
+    public static void main(String args[])
+    {
+        if(true){
+            System.out.println("yes");
+
+        }
+        if(false){
+            System.out.println("no");
+
+        }   }
+}
