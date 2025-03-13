78a47cb33b2a21e552790835c6f30cdf9eabbfad
francissusma <susmafrancis2005@gmail.com>
2025-03-13 14:05:26 +0530

    3D done

---
+++ 3D/MultiArr.java
@@ -0,0 +1,9 @@
+public class MultiArr
+{
+
+    public static void main(String args[])
+    {
+        int num[][][]={{{10,200}},{{100,300,65},{20,30,40}},{{50}}};
+        System.out.println(num[1][0][2]);
+    }
+}
