0d09f8d2bbb4492947f84614ad7c9c021fe9e69f
francissusma <susmafrancis2005@gmail.com>
2025-03-13 11:12:09 +0530

    positive and ngative done

---
+++ PositiveNegative/PositiveNegative.java
@@ -0,0 +1,16 @@
+public class PositiveNegative
+{
+    public static void main(String args[])
+    { 
+        int num=25;
+        if(num>=0)
+        {
+            System.out.println("it is positive");
+        }
+        else
+        {
+            System.out.println("it is negative");
+        }
+    }
+
+}
