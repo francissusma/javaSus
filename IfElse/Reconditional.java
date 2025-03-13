c64cb2b2c07b630605656a80a4ecd7dd0ddffd57
francissusma <susmafrancis2005@gmail.com>
2025-03-13 10:58:55 +0530

    ifelse

---
+++ IfElse/Reconditional.java
@@ -0,0 +1,15 @@
+public class Reconditional
+{
+    public static void main(String args[])
+    {
+        if(true)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+
+}
