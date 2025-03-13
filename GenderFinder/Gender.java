3db84698529894a1633180d078ea14b4bd45c614
francissusma <susmafrancis2005@gmail.com>
2025-03-13 11:27:01 +0530

    gender is revealed

---
+++ GenderFinder/Gender.java
@@ -0,0 +1,16 @@
+public class Gender
+{
+    public static void main(String args[])
+    { 
+    char gender='m';
+        if(gender=='m')
+        {
+            System.out.println("it is male");
+        }
+        else
+        {
+            System.out.println("it is female");
+        }
+    }
+
+}
