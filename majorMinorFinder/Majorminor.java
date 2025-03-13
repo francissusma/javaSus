4df3def1e52e6d459e7b77d4cb3bc56e87a15bd2
francissusma <susmafrancis2005@gmail.com>
2025-03-13 11:07:06 +0530

    majorminor

---
+++ majorMinorFinder/Majorminor.java
@@ -0,0 +1,16 @@
+public class Majorminor
+{
+    public static void main(String args[])
+    { 
+        int age =25;
+        if(age>=18)
+        {
+            System.out.println("she is major");
+        }
+        else
+        {
+            System.out.println("she is minor");
+        }
+    }
+
+}
