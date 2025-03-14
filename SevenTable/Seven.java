534364c84a6d9812e024e823267636405d891ce2
francissusma <susmafrancis2005@gmail.com>
2025-03-14 10:22:48 +0530

    seventable

---
+++ SevenTable/Seven.java
@@ -0,0 +1,14 @@
+public class Seven
+{
+    public static void main(String args[])
+    {
+        int i=7;
+        int j=1;
+        while(i<=70)
+        {
+            System.out.println(j+"*7="+i);
+            i=i+7;
+            j++;
+        }
+    }
+}
