80507d36c4f112247ee251cd8841b1cd0c3cb000
francissusma <susmafrancis2005@gmail.com>
2025-03-15 10:39:56 +0530

    constructor done

---
+++ Constructor/Constructor.java
@@ -0,0 +1,13 @@
+public class Constructor
+{
+public void Constructor()
+        {
+            System.out.println("hi");
+        }
+public static void main(String args[])
+    {
+        Constructor obj =new Constructor();
+        obj.Constructor();
+        
+    }
+}
